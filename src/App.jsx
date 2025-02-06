5e99b4278cf70cf732b4c89578c5340a2e0ae4e6
Farok-Hossain <farokafs7@gmail.com>
2025-02-06 09:59:45 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { motion, useAnimation, useInView, useScroll, useTransform } from "framer-motion";
+import {
+  motion,
+  useAnimation,
+  useInView,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 import { useEffect, useRef } from "react";
 
 const gridContainerVariants = {
@@ -27,27 +33,27 @@ const svgIconVariants = {
 
 const App = () => {
   const { scrollYProgress: completionProgress } = useScroll();
-  
+
   const containerRef = useRef(null);
 
-  const isInView = useInView(containerRef, {once: true});
+  const isInView = useInView(containerRef, { once: true });
   const mainControls = useAnimation();
-  const {scrollYProgress} = useScroll({
+  const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start end", "end end"]
+    offset: ["start end", "end end"],
   });
 
   const paragraphOneValue = useTransform(
     scrollYProgress,
     [0, 1],
     ["-100%", "0%"]
-  )
+  );
 
   const paragraphTwoValue = useTransform(
     scrollYProgress,
     [0, 1],
     ["100%", "0%"]
-  )
+  );
 
   useEffect(() => {
     if (isInView) {
@@ -94,7 +100,7 @@ const App = () => {
             transition={{
               duration: 5,
               ease: "easeInOut",
-              repeat: 2,
+              repeat: Infinity,
               repeatDelay: 1,
             }}
           />
@@ -110,7 +116,7 @@ const App = () => {
               backgroundColor: "#d1d5db",
               color: "black",
             }}
-            transition={{ bounceDamping: 10, bounceStiffness: 600 }}
+            transition={{ bounceDamping: 10, bounceStiffness: 500 }}
             className="bg-emerald-600 w-1/2 py-4  rounded-lg text-2xl text-gray-100 font-light tracking-wide"
           >
             Subscribe
@@ -180,19 +186,32 @@ const App = () => {
         </motion.div>
       </motion.section>
       <section className="flex flex-col gap-10 mb-10" ref={containerRef}>
-          <motion.h1 className="text-5xl tracking-wide text-slate-100 text-center"
+        <motion.h1
+          className="text-5xl tracking-wide text-slate-100 text-center"
           animate={mainControls}
           initial="hidden"
           variants={{
             hidden: { opacity: 0, y: 75 },
             visible: { opacity: 1, y: 0 },
           }}
-          transition={{delay: 0.3}}
-          >
-            Just Keep Scrolling
-          </motion.h1>
-          <motion.p style={{translateX: paragraphOneValue}} className="text-slate-100 font-thin text-4xl w-1/2 mx-auto">This is a basic framer motion animation on how to get up and running with Framer Motion with some TailwindCSS</motion.p>
-          <motion.p style={{translateX: paragraphTwoValue}} className="text-slate-100 font-thin text-4xl w-1/2 mx-auto">Have fun playing with Framer Motion. It is a ver powerful library, when used properly. Add some life to your websites.</motion.p>
+          transition={{ delay: 0.3 }}
+        >
+          Just Keep Scrolling
+        </motion.h1>
+        <motion.p
+          style={{ translateX: paragraphOneValue }}
+          className="text-slate-100 font-thin text-4xl w-1/2 mx-auto"
+        >
+          This is a basic framer motion animation on how to get up and running
+          with Framer Motion with some TailwindCSS
+        </motion.p>
+        <motion.p
+          style={{ translateX: paragraphTwoValue }}
+          className="text-slate-100 font-thin text-4xl w-1/2 mx-auto"
+        >
+          Have fun playing with Framer Motion. It is a ver powerful library,
+          when used properly. Add some life to your websites.
+        </motion.p>
       </section>
     </div>
   );
